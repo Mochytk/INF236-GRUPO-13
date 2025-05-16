6f977e743f7b7ab5f2003f0893e8439908b5b472
José Yañez <57007976+MasterJayanX@users.noreply.github.com>
2025-05-16 17:16:58 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-x-terminal-emulator -- bash -c "cd app/paes && python3 manage.py runserver; exec bash"
-
-# Start npm in the frontend directory in another new terminal
-x-terminal-emulator -- bash -c "cd app/vue && npm start; exec bash"
